9dd399147a18edb70841721c7a74444729a1c1ff
Abhishek <abhishek1.singh@course5i.com>
2025-07-26 14:00:26 +0530

    Updated config

--- config/config.py
+++ config/config.py
@@ -1,4 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Configuration for Azure Bot App
 AZURE_BOT_APP_CONFIG = {
-    "azure_bot_app_id": "your-bot-app-id",
-    "azure_app_bot_password": "your-bot-app-password"
+    "azure_bot_app_id" : os.getenv("AZURE_BOT_APP_ID"),
+    "azure_app_bot_password" : os.getenv("AZURE_BOT_APP_PASSWORD")
 }
